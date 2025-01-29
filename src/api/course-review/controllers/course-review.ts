9d4965a9c021523c519b23bae96f61a44c35f99a
th3bossc <diljith2003@gmail.com>
2025-01-29 17:43:57 +0530

    modified course review api calls

--- src/api/course-review/controllers/course-review.ts
+++ src/api/course-review/controllers/course-review.ts
@@ -4,4 +4,21 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::course-review.course-review');
+export default factories.createCoreController('api::course-review.course-review', ({ strapi }) => ({
+    async find(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            populate: ['courseId', 'reviewed_in_sem'],
+        }
+        return super.find(ctx);
+    },
+    async findOne(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            populate: ['courseId', 'reviewed_in_sem'],
+        }
+
+        return super.findOne(ctx);
+    }
+
+}));
--- src/api/course/controllers/course.ts
+++ src/api/course/controllers/course.ts
@@ -4,4 +4,52 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::course.course');
+export default factories.createCoreController('api::course.course', ({ strapi }) => ({
+    async findOne(ctx) {
+        const query = {
+            ...ctx.query,
+            populate: {
+                course_reviews: {
+                    populate: ['reviewed_in_sem'],
+                }
+            },
+        }
+
+        ctx.query = query;
+        return super.findOne(ctx);
+    },
+    async find(ctx) {
+        const filters = ctx.query.filters;
+        if (filters) {
+            const query = {
+                ...ctx.query,
+                populate: {
+                    course_reviews: {
+                        populate: ['reviewed_in_sem'],
+                    }
+                },
+            }
+
+            ctx.query = query;
+            return super.find(ctx);
+        }
+        else {
+            const courses = await super.find(ctx);
+
+            const enhancedCourses = await Promise.all(
+                courses.data.map(async (course) => {
+                    const reviewCount = await strapi.entityService.count('api::course-review.course-review', {
+                        filters: {
+                            courseId: course.id,
+                        },
+                    });
+                    course.reviewCount = reviewCount;
+                    return course;
+                })
+            );
+
+            courses.data = enhancedCourses;
+            return courses;
+        }
+    }
+}));
